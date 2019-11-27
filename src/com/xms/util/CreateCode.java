d90b0cd1896084e44952878fb58a3b1f5f81df37
git-sunyingzhan <oneselfwechat@163.com>
2019-11-27 19:12:46 +0800

    随机验证码图片

--- src/com/xms/util/CreateCode.java
+++ src/com/xms/util/CreateCode.java
@@ -15,6 +15,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * 生成随机图片验证码
+ * @author Administrator
+ *
+ */
 public class CreateCode extends HttpServlet {
 	@Override
 	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
